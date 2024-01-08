b60d7dbd158fe0aaecd6ad7bbd14ea09c59135bb
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-01-08 17:46:58 +0300

    fixed type errors

--- src/components/MovieCarousel/Movie.tsx
+++ src/components/MovieCarousel/Movie.tsx
@@ -14,7 +14,7 @@ const Movie = ({movies, title, isVertical} : Props) => {
       <h2><b>{title}</b></h2>
     <div className={styles.movies}>
         {movies?.map(movie => (
-            <MovieCard movie={movie}/>
+            <MovieCard key={movie.id} movie={movie}/>
             ))}
     </div>
             </div>
